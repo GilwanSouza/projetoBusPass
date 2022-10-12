363e4eb18c1be456fdf2141720f4d0676104d343
GilwanSouza <111159533+GilwanSouza@users.noreply.github.com>
2022-10-12 14:07:33 -0300

    1.0.0

--- buspass/src/com/br/buspass/LoginFuncionario.java
+++ buspass/src/com/br/buspass/LoginFuncionario.java
@@ -2,6 +2,7 @@ package com.br.buspass;
 import java.util.Scanner;
 
 import com.br.buspass.conexao.ConexaoBD;
+import com.br.buspass.Funcionario;
 
 // [RF 004] O sistema deve possibilitar o login para quem for viajar com o motorista com CPF ou matrícula e senha. 
 
@@ -11,32 +12,17 @@ public class LoginFuncionario {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        String log;
-        String senh;
-
-        // 1) Informe o seu login
         System.out.println("Login: ");
         String login = input.nextLine();
 
-        // 2) Informe a sua senha
         System.out.println("Senha: ");
         String senha = input.nextLine();
-        // 3) Armazenar em variaveis o login e a senha
 
-        // 4) Verificar se o login e a senha estão corretos
-        // Estamos comparando uma instância(objeto) com uma string
-        if(login.equals("claudio") && senha.equals("123.456.789-09")){
-            System.out.printf("Usuário %s logado com sucesso.", login);
-        }else{
-            System.out.println("Login ou senha inválidos!");
-        }
-
-    LoginF log_fun = new LoginF();
-    log_fun.setCpf(login);
+    Funcionario log_fun = new Funcionario();
+    log_fun.setCPF(login);
     log_fun.setSenha(senha);
 
     ConexaoBD.LoginFunc(log_fun);
-
 
     }
 }
--- buspass/src/com/br/buspass/conexao/ConexaoBD.java
+++ buspass/src/com/br/buspass/conexao/ConexaoBD.java
@@ -10,6 +10,7 @@ import java.sql.SQLException;
 import com.br.buspass.Aluno;
 import com.br.buspass.Funcionario;
 import com.br.buspass.Horario;
+import com.br.buspass.LoginFuncionario;
 import com.br.buspass.VagaVeiculo;
 import com.br.buspass.Veiculo;
 
@@ -24,19 +25,48 @@ public class ConexaoBD {
     private final static String user = "postgres";
     private final static String password = "admin";
     private static final String QUERY = "SELECT * FROM aluno, horario, veiculo";
+    private final String QUERY2 = "";
+
+    public static Funcionario LoginFunc(Funcionario login_fun) {
+        try (Connection connection = DriverManager.getConnection(url, user, password);
+                // Step 2:Create a statement using connection object
+                PreparedStatement preparedStatement = connection
+                        .prepareStatement("SELECT cpf, senha FROM funcionario WHERE cpf = ? and senha = ?;");) {
+            preparedStatement.setString(1, login_fun.getCPF());
+            preparedStatement.setString(2, login_fun.getSenha());
+            // Step 3: Execute the query or update query
+
+            ResultSet resultset = preparedStatement.executeQuery();
+
+            Funcionario fun_logado = new Funcionario();
+
+            if(resultset.next()){
+                
+                fun_logado.setCPF(resultset.getString("cpf"));
+                fun_logado.setSenha(resultset.getString("senha"));
+            } 
+           return fun_logado;
+
+            // Step 4: Process the ResultSet object.
+        } catch (SQLException e) {
+            printSQLException(e);
+        }
+        return null;
+    }
 
     public static void cadastroFuncionario(Funcionario funcionario) {
         try (Connection connection = DriverManager.getConnection(url, user, password);
-            // Step 2:Create a statement using connection object
-                PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO funcionario (nome, cpf, senha, id_vel) VALUES (?, ?, ?, ?);");) {
-                    preparedStatement.setString(1, funcionario.getNome());
-                    preparedStatement.setString(2, funcionario.getCPF());
-                    preparedStatement.setString(3, funcionario.getSenha());
-                    preparedStatement.setInt(4, funcionario.getId_veiculo());
-                // Step 3: Execute the query or update query
+                // Step 2:Create a statement using connection object
+                PreparedStatement preparedStatement = connection
+                        .prepareStatement("INSERT INTO funcionario (nome, cpf, senha, id_vel) VALUES (?, ?, ?, ?);");) {
+            preparedStatement.setString(1, funcionario.getNome());
+            preparedStatement.setString(2, funcionario.getCPF());
+            preparedStatement.setString(3, funcionario.getSenha());
+            preparedStatement.setInt(4, funcionario.getId_veiculo());
+            // Step 3: Execute the query or update query
             preparedStatement.executeUpdate();
             // Step 4: Process the ResultSet object.
-            
+
         } catch (SQLException e) {
             printSQLException(e);
         }
@@ -45,14 +75,15 @@ public class ConexaoBD {
 
     public static void cadastroVaga(VagaVeiculo vaga) {
         try (Connection connection = DriverManager.getConnection(url, user, password);
-            // Step 2:Create a statement using connection object
-                PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO vagas (vagas, id_vel) VALUES (?, ?);");) {
-                    preparedStatement.setInt(1, vaga.getVaga());
-                    preparedStatement.setInt(2, vaga.getId_Search());
-                // Step 3: Execute the query or update query
+                // Step 2:Create a statement using connection object
+                PreparedStatement preparedStatement = connection
+                        .prepareStatement("INSERT INTO vagas (vagas, id_vel) VALUES (?, ?);");) {
+            preparedStatement.setInt(1, vaga.getVaga());
+            preparedStatement.setInt(2, vaga.getId_Search());
+            // Step 3: Execute the query or update query
             preparedStatement.executeUpdate();
             // Step 4: Process the ResultSet object.
-            
+
         } catch (SQLException e) {
             printSQLException(e);
         }
@@ -61,17 +92,18 @@ public class ConexaoBD {
 
     public static void cadastroVeiculo(Veiculo veiculo) {
         try (Connection connection = DriverManager.getConnection(url, user, password);
-            // Step 2:Create a statement using connection object
-                PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO veiculo (ano, modelo, chassi, placa, id_vel) VALUES (?, ?, ?, ?, ?);");) {
-                    preparedStatement.setInt(1, veiculo.getAno());
-                    preparedStatement.setString(2, veiculo.getModelo());
-                    preparedStatement.setString(3, veiculo.getChassi());
-                    preparedStatement.setString(4, veiculo.getPlaca());
-                    preparedStatement.setInt(5, veiculo.getId_vel());
-                // Step 3: Execute the query or update query
+                // Step 2:Create a statement using connection object
+                PreparedStatement preparedStatement = connection.prepareStatement(
+                        "INSERT INTO veiculo (ano, modelo, chassi, placa, id_vel) VALUES (?, ?, ?, ?, ?);");) {
+            preparedStatement.setInt(1, veiculo.getAno());
+            preparedStatement.setString(2, veiculo.getModelo());
+            preparedStatement.setString(3, veiculo.getChassi());
+            preparedStatement.setString(4, veiculo.getPlaca());
+            preparedStatement.setInt(5, veiculo.getId_vel());
+            // Step 3: Execute the query or update query
             preparedStatement.executeUpdate();
             // Step 4: Process the ResultSet object.
-            
+
         } catch (SQLException e) {
             printSQLException(e);
         }
@@ -80,17 +112,18 @@ public class ConexaoBD {
 
     public static void cadastroAluno(Aluno aluno) {
         try (Connection connection = DriverManager.getConnection(url, user, password);
-            // Step 2:Create a statement using connection object
-                PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO aluno (nome, num_tel, cpf, senha, matricula) VALUES (?, ?, ?, ?, ?);");) {
-                    preparedStatement.setString(1, aluno.getNome());
-                    preparedStatement.setString(2, aluno.getNumero());
-                    preparedStatement.setString(3, aluno.getCpf());
-                    preparedStatement.setString(4, aluno.getSenha());
-                    preparedStatement.setInt(5, aluno.getMatricula());
-                // Step 3: Execute the query or update query
+                // Step 2:Create a statement using connection object
+                PreparedStatement preparedStatement = connection.prepareStatement(
+                        "INSERT INTO aluno (nome, num_tel, cpf, senha, matricula) VALUES (?, ?, ?, ?, ?);");) {
+            preparedStatement.setString(1, aluno.getNome());
+            preparedStatement.setString(2, aluno.getNumero());
+            preparedStatement.setString(3, aluno.getCpf());
+            preparedStatement.setString(4, aluno.getSenha());
+            preparedStatement.setInt(5, aluno.getMatricula());
+            // Step 3: Execute the query or update query
             preparedStatement.executeUpdate();
             // Step 4: Process the ResultSet object.
-            
+
         } catch (SQLException e) {
             printSQLException(e);
         }
@@ -98,16 +131,17 @@ public class ConexaoBD {
 
     public static void cadastroHorario(Horario horario) {
         try (Connection connection = DriverManager.getConnection(url, user, password);
-            // Step 2:Create a statement using connection object
-                PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO horario (numero, hr_ida, hr_volta, dt_viagem) VALUES (?, ?, ?, ?);");) {
-                    preparedStatement.setInt(1, horario.getNumero());
-                    preparedStatement.setString(2, horario.getHrIda());
-                    preparedStatement.setString(3, horario.getHrVolta());
-                    preparedStatement.setDate(4, Date.valueOf(horario.getDataViagem()));
-                // Step 3: Execute the query or update query
+                // Step 2:Create a statement using connection object
+                PreparedStatement preparedStatement = connection.prepareStatement(
+                        "INSERT INTO horario (numero, hr_ida, hr_volta, dt_viagem) VALUES (?, ?, ?, ?);");) {
+            preparedStatement.setInt(1, horario.getNumero());
+            preparedStatement.setString(2, horario.getHrIda());
+            preparedStatement.setString(3, horario.getHrVolta());
+            preparedStatement.setDate(4, Date.valueOf(horario.getDataViagem()));
+            // Step 3: Execute the query or update query
             preparedStatement.executeUpdate();
             // Step 4: Process the ResultSet object.
-            
+
         } catch (SQLException e) {
             printSQLException(e);
         }
